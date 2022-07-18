e327affbb4b0d51765daade32127a2d924dae845
Sziyi <3275157006@qq.com>
2022-07-18 15:14:12 +0800

    feat(button 封装): button

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: ["plugin:vue/essential", "@vue/standard"],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    quotes: "error", // 默认
-    quotes: "warn", // 修改为警告
-    quotes: "off", // 修改不校验
-    'space-before-function-paren': 'off'
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: 'error', // 默认
+    quotes: 'warn', // 修改为警告
+    quotes: 'off', // 修改不校验
+    'space-before-function-paren': 'off',
+    'vue/multi-word-component-names': 'off'
+  }
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <songButton>默认按钮</songButton>
+    <songButton type="primary">成功按钮</songButton>
+    <songButton type="danger">危险按钮</songButton>
+    <songButton type="warning" @click="handleSubmit">警告按钮</songButton>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      flag: false
+    }
+  },
+  components: {
+    songButton: () => import('../components/button/index.vue')
+  },
+  methods: {
+    handleSubmit() {
+      this.flag = true
+      setTimeout(() => {
+        console.log('123')
+        this.flag = false
+      }, 3000)
+    }
+  }
+}
+</script>
