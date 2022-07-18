5ab9d02df5bbad80eedbbfc4783db67e991587e8
Sziyi <3275157006@qq.com>
2022-07-18 14:52:24 +0800

    feat(a): a

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: "@babel/eslint-parser",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: "error", // 默认
+    quotes: "warn", // 修改为警告
+    quotes: "off", // 修改不校验
+    'space-before-function-paren': 'off'
+  },
+};
